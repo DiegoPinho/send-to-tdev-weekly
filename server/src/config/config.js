9cc7b5cd6a58a334e67f691eb8860282b7e66ebf
Diego Martins de Pinho <diegomartinsdepinho@msn.com>
2017-04-12 10:51:06 -0300

    fix api_url link

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,5 +1,4 @@
 module.exports = {
     'GOOGL_API_KEY': 'AIzaSyAHbx5_7GRWOFvyaJXQk7CG0y_NwMbNSlA',
-    // 'API_URL' : 'https://tdevweekly-api.herokuapp.com'
-    'API_URL' : 'http://localhost:8080'
+    'API_URL' : 'https://tdevweekly-api.herokuapp.com'
 }
